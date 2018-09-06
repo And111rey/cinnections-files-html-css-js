c21b2c41e64e286991953a6cb79e8aa4e0f2bd13
And111rey <Samu111ray@gmail.com>
2018-09-06 16:37:09 +0300

    способ загрузки на репозиторий

---
+++ пара HTML+CSS+Js+jQuwery/script.js
@@ -0,0 +1,17 @@
+$('document').ready(function() {
+
+
+	$('body').append('<a href="http://google.com"> перейти в гугл</a>');
+
+	
+
+	
+	$('p').fadeOut(3000);// text <p> will disappear after 3 seconds - текст исчезнет через 3 сек
+	$('p').fadeIn(3000);//text will appear in 3 seconds - текст появиться через 3сек
+  
+
+
+
+
+
+})
